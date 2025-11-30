631739cab356295360ae04f06312ba748cc3c620
ashishchaubey <ashish.23b0121172@abes.ac.in>
2025-11-30 15:37:36 +0530

    all commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function Dashboard() {
   const[data , setData] = useState([]);
+  const[cart , setCart] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -14,8 +15,33 @@ function Dashboard() {
     fetchData();
   }, []);
 
+  function shoppingCart(ele){
+     setCart(data=>[...data,ele])
+     alert("Item added successfully");
+     
+
+    // alert(ele.title);
+  }
+  const totalAmount = cart.reduce((sum, item) => sum + item.price, 0);
+
+
   return (
     <>
+
+    <div>
+      {
+        <h2>Total Amount: {totalAmount.toFixed(2)}</h2>
+      }
+      {
+           
+        cart.length==0?(<h4>Cart is empty</h4>):
+        (
+          cart.map((ele)=>(
+            <h3 style={{backgroundColor:'cayn'}}>{ele.title}</h3>
+          ))
+        )
+      }
+    </div>
 
     {
       data.length===0?(<h2>Data could not fetch</h2>):(
@@ -23,10 +49,12 @@ function Dashboard() {
           {
 
              data.map((ele)=>(
-            <div style={{ height:'300px',width:'300px'}}>
+            <div style={{ height:'500px',width:'300px'}}>
               <img src={ele.image} height={100} width={100}  />
               <h3>{ele.title}</h3>
-              <button>Add to Cart</button>
+              <h1>{ele.price}</h1>
+              <p>{ele.description}</p>
+              <button onClick ={()=>shoppingCart(ele)}>Add to Cart</button>
             </div>
           ))
 
